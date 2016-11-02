dbac55bd696b5e3c25ea49cd4c8aaa9e536b9b59
artist <artist.web.template@gmail.com>
2016-11-02 10:45:31 -0700

    all

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -21,7 +21,7 @@ jQuery(document).ready(function ($) {
             parts = full_url.split('#'),
             trgt = parts[1],
             target_offset = $('#' + trgt).offset(),
-            target_top = target_offset.top - 40;
+            target_top = target_offset.top - 80;
 
         $('html, body').animate({ scrollTop: target_top }, 1000);
 
