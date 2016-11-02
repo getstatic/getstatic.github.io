f1b753d7a6a7d838e632a85f99ac82aab245e2e1
artist <artist.web.template@gmail.com>
2016-11-02 10:12:12 -0700

    all

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -62,7 +62,7 @@ jQuery(document).ready(function ($) {
 
         for (var i = 0; i < aArray.length; i++) {
             var theID = aArray[i];
-            var theURL = "http://getstatic.github.io/";
+            var theURL = "https://getstatic.github.io/";
             var divPos = $(theID).offset().top - 80; // get the offset of the div from the top of page
             var divHeight = $(theID).height(); // get the height of the div in question
             if (windowPos >= divPos && windowPos < divPos + divHeight) {
